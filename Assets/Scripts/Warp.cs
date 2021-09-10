1723a8b82bbbeb82105a3c48a4c39f4d1f356567
CBBA <theporkchopsxd@gmail.com>
2021-09-10 11:16:19 +0200

    first draft warping logic

--- Assets/Scripts/Warp.cs
+++ Assets/Scripts/Warp.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Warp : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform Player;
+    public float MinDistance;
+    public float MinAngleDelta;
+    public Color[] ColorCycle;
+    private bool cycled;
+    private int cycleIndex;
+    private MeshRenderer mr;
     void Start()
     {
-        
+        mr = GetComponent<MeshRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Vector3.Distance(transform.position, Player.position) < MinDistance)
+        {
+            Vector3 angleToPlayer = (transform.position - Player.position).normalized;
+            Vector3 vAngleDelta = Player.forward - angleToPlayer;
+            if (vAngleDelta.magnitude > MinAngleDelta || vAngleDelta.magnitude < -MinAngleDelta)
+            {
+                if (!cycled)
+                {
+                    cycled = true;
+                    mr.material.color = ColorCycle[cycleIndex++];
+                }
+            }
+            else cycled = false;
+
+        }
     }
 }
