4809a144d21cf161485d16e4daeb5ffd8c065394
Henrik <theporkchopsxd@gmail.com>
2021-09-15 17:53:08 +0200

    Follow

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform Player;
+    public float MinDistance;
+    public float MinAngleDelta;
+    public float FollowDistance;
+    public Vector3 CollideCheckBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(transform.position, Player.position) > MinDistance)
+        {
+            Vector3 angleToPlayer = (transform.position - Player.position).normalized;
+            Vector3 vAngleDelta = Player.forward - angleToPlayer;
+            if (vAngleDelta.magnitude > MinAngleDelta || vAngleDelta.magnitude < -MinAngleDelta)
+            {
+                Vector3 followPosition = Player.position - (Player.forward * FollowDistance);
+                if (Physics.OverlapBox(followPosition, CollideCheckBox, Player.rotation, 0, QueryTriggerInteraction.Ignore).Length == 0)
+                {
+                    transform.position = followPosition;
+                    transform.rotation = Player.rotation;
+                }
+                else
+                {
+                    Debug.Log("Overlap test failed");
+                }
+            }
+        }
+    }
+}
