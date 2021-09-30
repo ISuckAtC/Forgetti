c038aca037668e5f1b131b0c916ebf0f18a9ec26
Henrik <theporkchopsxd@gmail.com>
2021-09-30 12:10:53 +0200

    tp crafting

--- Assets/Scripts/BasicController.cs
+++ Assets/Scripts/BasicController.cs
@@ -12,19 +12,21 @@ public class BasicController : MonoBehaviour
     public float PickupRange;
     public float HoldDistance;
     private GameObject heldObject;
+    static public GameObject Player;
 
     void Start()
     {
+        Player = gameObject;
         rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
     }
     // Update is called once per frame
     void Update()
     {
-        /*Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * RotationSpeed, Space.Self);
         PlayerCamera.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, 0) * (RotationSpeed / 3), Space.Self);
-        rb.velocity = (((transform.right * movement.x) + (transform.forward * movement.y)).normalized * MovementSpeed) + new Vector3(0, rb.velocity.y, 0);*/
+        rb.velocity = (((transform.right * movement.x) + (transform.forward * movement.y)).normalized * MovementSpeed) + new Vector3(0, rb.velocity.y, 0);
 
 
         if (Input.GetMouseButtonDown(0))
--- Assets/Scripts/BookshelfCrafting.cs
+++ Assets/Scripts/BookshelfCrafting.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BookshelfCrafting : CraftingIngredient
@@ -12,6 +13,11 @@ public class BookshelfCrafting : CraftingIngredient
 
     public override void Craft(Collision c)
     {
-        
+        BasicController.Player.transform.Translate(new Vector3(0, 10, 0), Space.World);
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Pickup").Where(x => x.name == c.transform.name).ToArray();
+        foreach (GameObject o in objects)
+        {
+            o.transform.localPosition = new Vector3(-1.729f, 1.07f, -13.306f);
+        }
     }
 }
