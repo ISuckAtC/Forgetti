37afac4f667814b891b8693f72530efe2e753869
Henrik <theporkchopsxd@gmail.com>
2021-10-04 11:25:03 +0200

    tasks

--- Assets/Scripts/CraftAndTP.cs
+++ Assets/Scripts/CraftAndTP.cs
@@ -6,7 +6,7 @@ public class CraftAndTP : CraftingIngredient
 {
     public override IEnumerator Craft(Collision c, float delay = 0)
     {
-        GameObject prefab = Reactions[c.transform.name];
+        GameObject prefab = Reactions[c.transform.name].reaction;
         Vector3 position = c.contacts[0].point;
         Quaternion rotation = Quaternion.Euler(c.contacts[0].normal);
 
@@ -14,6 +14,9 @@ public class CraftAndTP : CraftingIngredient
         yield return new WaitForSeconds(delay);
         GameObject g = Instantiate(prefab, position, rotation);
         g.GetComponent<Rigidbody>().velocity += new Vector3(0, ReactionForce, 0);
+
+        ClipBoardController.ClipBoardCtrl.UpdateJournal(Reactions[c.transform.name].task);
+
         Destroy(c.gameObject);
         Destroy(gameObject);
         BasicController.Player.transform.Translate(new Vector3(0, 10, 0), Space.World);
