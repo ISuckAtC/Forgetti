ceb91017f2a1396b837300c2f51e7b1c26c019b7
WizyWoo <kevin.hagfors@gmail.com>
2021-10-18 13:55:25 +0200

    h0i8h

--- Assets/Scripts/UI&Menus/JournalController.cs
+++ Assets/Scripts/UI&Menus/JournalController.cs
@@ -3,23 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class JournalController : MonoBehaviour
+public class JournalController : MonoBehaviour , IInteractable
 {
 
     public static JournalController main;
     [TextArea]
     public string HowToUse;
+    public Transform JournalAnchor;
+    public GameObject JournalVisuals;
     public Color FinishedTaskColour, Unfinished;
     public GameObject[] TaskTexts;
     public Vector3 WarpPos;
+    private Rigidbody rb;
     private Quaternion WarpRot;
+    private bool journalActive, journalOnPlayer;
+    public GameObject[] Links { get; set;}
 
     private void Start()
     {
 
         main = this;
+        rb = gameObject.GetComponent<Rigidbody>();
         WarpPos = transform.position + Vector3.up * 10;
         WarpRot = transform.rotation;
+        Links = new GameObject[0];
+
+    }
+
+    private void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Tab) && journalOnPlayer)
+            ToggleJournal();
+
+        if(Input.GetKeyDown(KeyCode.E) && journalOnPlayer)
+            PutDownJournal();
+
+    }
+
+    private void PutDownJournal()
+    {
+
+        transform.parent = null;
+        rb.constraints = RigidbodyConstraints.None;
+        rb.detectCollisions = true;
+        journalOnPlayer = false;
+        JournalVisuals.SetActive(true);
+        transform.position = Camera.main.transform.forward + Camera.main.transform.position;
+
+    }
+
+    private void ToggleJournal()
+    {
+
+        journalActive = !journalActive;
+        JournalVisuals.SetActive(journalActive);
 
     }
 
@@ -64,6 +102,20 @@ public class JournalController : MonoBehaviour
             }
 
         }*/
+
+    }
+
+    public void Interact(bool chain = false)
+    {
+
+        transform.parent = JournalAnchor;
+        transform.position = JournalAnchor.position;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        rb.detectCollisions = false;
+        transform.LookAt(Camera.main.transform, Vector3.up);
+        journalOnPlayer = true;
+        journalActive = false;
+        ToggleJournal();
 
     }
 
